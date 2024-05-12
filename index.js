59afb8f2c79aad751fcf61a7e0d8a0faded72af9
NhsTarek <htarek275@gmail.com>
2024-05-12 21:10:32 +0600

    created jobs api

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1qruita.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-console.log(process.env.DB_PASS);
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
@@ -28,7 +28,17 @@ const client = new MongoClient(uri, {
   });
   async function run() {
     try {
-      // Connect the client to the server	(optional starting in v4.7)
+     const jobsCollection = client.db('jobPedia').collection('jobs')
+
+    // getting all jobs data
+     
+    app.get('/jobs', async (req, res) =>{
+      const result = await jobsCollection.find().toArray();
+      res.send(result);
+    })
+
+
+
  
       // Send a ping to confirm a successful connection
       await client.db("admin").command({ ping: 1 });
