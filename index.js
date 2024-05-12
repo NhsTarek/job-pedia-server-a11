0f8242851eb9890b8282921280c75addc01e130e
NhsTarek <htarek275@gmail.com>
2024-05-12 22:14:32 +0600

    single job data api created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -34,6 +34,16 @@ const client = new MongoClient(uri, {
      
     app.get('/jobs', async (req, res) =>{
       const result = await jobsCollection.find().toArray();
+      res.send(result);
+    })
+
+
+    // getting single job data
+
+    app.get('/job/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await jobsCollection.findOne(query);
       res.send(result);
     })
 
