0061034659ae7474bbae329e21aa2303332d3243
NhsTarek <htarek275@gmail.com>
2024-05-14 00:40:32 +0600

    created  update a job api

--- index.js
+++ index.js
@@ -83,10 +83,25 @@ const client = new MongoClient(uri, {
     app.delete('/job/:id', async (req, res) =>{
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}
-      const result = await jobsCollection.find(query).toArray();
+      const result = await jobsCollection.deleteOne(query);
       res.send(result);
     })
 
+
+    // update a job
+    app.put('/job/:id', async(req, res) =>{
+      const id = req.params.id;
+      const jobData = req.body;
+      const query = {_id: new ObjectId(id)};
+      const options = {upsert: true};
+      const updateDoc = {
+        $set:{
+          ...jobData,
+        },
+      }
+      const result = await jobsCollection.updateOne(query, updateDoc, options);
+      res.send(result);
+    })
 
  
       // Send a ping to confirm a successful connection
